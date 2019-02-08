cbfb1ab54a6505f12606d9418bff45be008d9bd3
Michael A. Gonzalez <genomics.geek.04.22@gmail.com>
2019-02-08 10:57:49 -0500

    Refactored tests to use py.test fixtures correctly

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -1,57 +1,58 @@
 # -*- coding: utf-8 -*-
 from model_mommy import mommy
+import pytest
 
 
+@pytest.fixture
 def Ontology():
-    return mommy.make(
-        'phenotype_ontologies.Ontology',
-        type=1,
-        label='Ontology',
-        active=True,
-    )
+    def _func(**kwargs):
+        return mommy.make('phenotype_ontologies.Ontology', **kwargs)
+    return _func
 
 
+@pytest.fixture
 def Term():
-    return mommy.make(
-        'phenotype_ontologies.Term',
-        ontology=Ontology(),
-        identifier='identifier',
-        label='Term',
-        description='Term',
-        alternate_ids='1,2',
-        created_by='created_by',
-        created='created',
-    )
+    def _func(ontology=None, **kwargs):
+        if ontology is None:
+            ontology = mommy.make('phenotype_ontologies.Ontology', label='Ontology')
+        return mommy.make('phenotype_ontologies.Term', ontology=ontology, **kwargs)
+    return _func
 
 
+@pytest.fixture
 def Synonym():
-    return mommy.make(
-        'phenotype_ontologies.Synonym',
-        term=Term(),
-        description='Synonym',
-        scope=1,
-    )
+    def _func(term=None, **kwargs):
+        if term is None:
+            ontology = mommy.make('phenotype_ontologies.Ontology', type=1)
+            term = mommy.make('phenotype_ontologies.Term', ontology=ontology, identifier='Term')
+        return mommy.make('phenotype_ontologies.Synonym', term=term, **kwargs)
+    return _func
 
 
+@pytest.fixture
 def CrossReference():
-    return mommy.make(
-        'phenotype_ontologies.CrossReference',
-        term=Term(),
-        source=1,
-        source_value='source_value',
-    )
+    def _func(term=None, **kwargs):
+        if term is None:
+            ontology = mommy.make('phenotype_ontologies.Ontology', type=1)
+            term = mommy.make('phenotype_ontologies.Term', ontology=ontology, identifier='Term')
+        return mommy.make('phenotype_ontologies.CrossReference', term=term, **kwargs)
+    return _func
 
 
-def Relationship():
-    return mommy.make(
-        'phenotype_ontologies.Relationship',
-        type=RelationshipType(),
-        term=Term(),
-    )
-
-
+@pytest.fixture
 def RelationshipType():
-    return mommy.make(
-        'phenotype_ontologies.RelationshipType',
-        label='RelationshipType'
-    )
+    def _func(term=None, **kwargs):
+        return mommy.make('phenotype_ontologies.RelationshipType', **kwargs)
+    return _func
+
+
+@pytest.fixture
+def Relationship():
+    def _func(term=None, type=None, **kwargs):
+        if term is None:
+            ontology = mommy.make('phenotype_ontologies.Ontology', type=1)
+            term = mommy.make('phenotype_ontologies.Term', ontology=ontology, identifier='Term')
+        if type is None:
+            type = mommy.make('phenotype_ontologies.RelationshipType', label='RelationshipType')
+        return mommy.make('phenotype_ontologies.Relationship', term=term, type=type, **kwargs)
+    return _func
